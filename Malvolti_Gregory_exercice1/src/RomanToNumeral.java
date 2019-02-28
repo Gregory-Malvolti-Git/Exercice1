8d95b92ed61a711a4010c028921bf14ff9267b44
Gregory-Malvolti-Git <37400732+Gregory-Malvolti-Git@users.noreply.github.com>
2019-02-28 13:59:48 +0100

    Changement de la fonction pour qu'elle interprète le nombre de base.

--- Malvolti_Gregory_exercice1/src/RomanToNumeral.java
+++ Malvolti_Gregory_exercice1/src/RomanToNumeral.java
@@ -1,37 +1,47 @@
 
 public class RomanToNumeral {
-	public Integer getNumeral(String nbRomain) {
+	public int getNombreDeBase(char c) {
 		Integer nombre = 0;
-		if(nbRomain == "") {
+		if(c == ' ') {
 			return nombre;
 		}
-		if(nbRomain == "I") {
+		if(c == 'I') {
 			nombre = 1;
 			return nombre;
 		}
-		if(nbRomain == "V") {
+		if(c == 'V') {
 			nombre = 5;
 			return nombre;
 		}
-		if(nbRomain == "X") {
+		if(c == 'X') {
 			nombre = 10;
 			return nombre;
 		}
-		if(nbRomain == "L") {
+		if(c == 'L') {
 			nombre = 50;
 			return nombre;
 		}
-		if(nbRomain == "C") {
+		if(c == 'C') {
 			nombre = 100;
 			return nombre;
 		}
-		if(nbRomain == "D") {
+		if(c == 'D') {
 			nombre = 500;
 			return nombre;
 		}
-		if(nbRomain == "M") {
+		if(c == 'M') {
 			nombre = 1000;
 			return nombre;
+		}
+		return nombre;
+	}
+	
+	
+	
+	public int getNumeral(String nbRomain) {
+		int nombre = 0; 
+		for(int i =0; i<=nbRomain.length(); i++) {
+			nombre += getNombreDeBase(nbRomain.charAt(i));
 		}
 		return nombre;
 	}
