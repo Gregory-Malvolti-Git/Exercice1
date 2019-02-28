c51215823937c4f9ed27bb5071bba2915348db93
Gregory-Malvolti-Git <37400732+Gregory-Malvolti-Git@users.noreply.github.com>
2019-02-28 13:44:04 +0100

    Ajout de la fonction getNumeral dans la classe RomanToNumeral.

--- Malvolti_Gregory_exercice1/src/RomanToNumeral.java
+++ Malvolti_Gregory_exercice1/src/RomanToNumeral.java
@@ -1,4 +1,38 @@
 
 public class RomanToNumeral {
-
+	public Integer getNumeral(String nbRomain) {
+		Integer nombre = 0;
+		if(nbRomain == "") {
+			return nombre;
+		}
+		if(nbRomain == "I") {
+			nombre = 1;
+			return nombre;
+		}
+		if(nbRomain == "V") {
+			nombre = 5;
+			return nombre;
+		}
+		if(nbRomain == "X") {
+			nombre = 10;
+			return nombre;
+		}
+		if(nbRomain == "L") {
+			nombre = 50;
+			return nombre;
+		}
+		if(nbRomain == "C") {
+			nombre = 100;
+			return nombre;
+		}
+		if(nbRomain == "D") {
+			nombre = 500;
+			return nombre;
+		}
+		if(nbRomain == "M") {
+			nombre = 1000;
+			return nombre;
+		}
+		return nombre;
+	}
 }
